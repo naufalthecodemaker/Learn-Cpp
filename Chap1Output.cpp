0233d25a418381d302e1d5f96f85dd7626bb219c
naufalthecodemaker <naufalrahman867@gmail.com>
2025-08-26 22:54:39 +0700

    add chap 1 -> output

---
+++ Chap1Output.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+  std::cout << "first output" << std::endl; // endl makes new line and flush the output buffer
+  std::cout << "second output";
+
+  // this is a comment
+  return 0;
+}
