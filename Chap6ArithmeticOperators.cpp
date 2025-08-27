9816eb56f751b844f2482bd38f6bf8da7c859923
naufalthecodemaker <naufalrahman867@gmail.com>
2025-08-27 09:51:50 +0700

    add chap 6 -> arithmetic operators

---
+++ Chap6ArithmeticOperators.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  // (+ - * / %)
+
+  // example
+  double width = 20.3, length = 12.3, area;
+
+  area = width * length;
+
+  cout << "Area: " << area << " cm^2" << endl;
+
+  return 0;
+}
