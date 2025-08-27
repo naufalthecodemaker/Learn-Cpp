bc5d9b34e563d3cef3f8380e861ede8f62dd4dcd
naufalthecodemaker <naufalrahman867@gmail.com>
2025-08-27 09:01:06 +0700

    add chap 3 -> const

---
+++ Chap3Const.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+  /*
+    The const keyword specifies that a variable's value is constant 
+    and tells the compiler to prevent anything from modifying it
+    (read only)
+  */
+
+  //example
+  const double PI = 3.14159; 
+  const int LIGHT_SPEED = 2997992458;
+  return 0;
+}
